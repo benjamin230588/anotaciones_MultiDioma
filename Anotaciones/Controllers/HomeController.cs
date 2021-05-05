f21e3453c54bd5db5556ab26252a021e7129f3e5
benjamin <riders_23x@hotmail.com>
2021-05-04 22:19:50 -0500

    configuracion mapper

--- Anotaciones/Controllers/HomeController.cs
+++ Anotaciones/Controllers/HomeController.cs
@@ -27,5 +27,43 @@ namespace Anotaciones.Controllers
 
             return View();
         }
+        /*
+         consola Maper
+        1.primero agregar mapper como referencia luego 
+        Console.WriteLine("Hello World!");
+           
+            departamento depa = new departamento();
+            depa.correo = "benja";
+            depa.nombre = "pepe";
+
+            distrito dis = new distrito();
+
+            var config = new MapperConfiguration(cfg => {
+                cfg.CreateMap<departamento, distrito>();
+            });
+            IMapper iMapper = config.CreateMapper();
+            dis = iMapper.Map<departamento, distrito>(depa);
+
+            Console.Read();
+
+        class departamento
+    {
+
+        public string correo { get; set; }
+        public string nombre { get; set; }
+
+
+    }
+
+    class distrito
+    {
+        public string correo { get; set; }
+        public string nombre { get; set; }
+
+        public string habitantes { get; set; }
+        public string cantidad { get; set; }
+
+    }
+         */
     }
 }
